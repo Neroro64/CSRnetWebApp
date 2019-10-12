d0fe800661e45910e102c51373eda10696a0e8c6
zhien <limzhien@hotmail.com>
2019-10-12 15:42:23 +0800

    general layout. haven't done rating system

--- Web App/webapp/src/pages/places.js
+++ Web App/webapp/src/pages/places.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:248c8184cd09af65afb5bc885ba0ccd24ab5241d5cd2bf9d51de3934567a9f93
-size 141
+oid sha256:8d410e23d57bfb6c5946a07c2ef2b600e3f9185968e7db95d810c839d4dd85ef
+size 1021
