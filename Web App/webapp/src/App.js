9854686503a42fbcc65bf33fe5b2edc5afb5fd58
Albert <github@albert.sh>
2019-10-16 22:38:31 +0800

    Fix import bug

--- Web App/webapp/src/App.js
+++ Web App/webapp/src/App.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:372b74247cd644ead672b8206d91505db9d536af3392fb7868910505dd7f5f7b
+oid sha256:eab45a5c043e810192fb90d4b68c32c0023d96da9e6159fff7eac60e491ecbae
 size 1301
