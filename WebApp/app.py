f90e42f428c3c12c49fca9a94a5b940ac36d0a8c
NuoC <nuoc@kth.se>
2019-10-17 19:16:13 +0800

    Minor fix

--- WebApp/app.py
+++ WebApp/app.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:3a1ef56dd54075e01d67a44ee5dd3028c3dfb7d2f96a90ea75aead93015bd249
-size 1497
+oid sha256:a7c69c3df440ddcf08900255b8061d72adfb6fc629cd307684dd6d1a26d86c0a
+size 1539
--- WebApp/report.py
+++ WebApp/report.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:db0ea90724290b4da6e930a7ad8d6ea417d98486b9bcd7dd49e7e842393d950d
-size 992
+oid sha256:35e8c6704e0f36775f73c01676765cd5b7a12c4445fff197515de768756aea9e
+size 953
